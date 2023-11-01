ea1af86f0f940e115ea189da343866cab86361bb
Helge Penne <hpenne@gmail.com>
2023-11-01 15:43:34 +0100

    Error handling strategies. Special member functions (need testing).

--- src/owned_ptr.h
+++ src/owned_ptr.h
@@ -8,26 +8,37 @@
 #include <cassert>
 #include <memory>
 
-template<typename T>
-class dep_ptr;
-
-template<typename T>
-class owned_ptr {
-public:
-    // ToDo: A get_dep could be useful, especially if error handling strategies are introduced
-
-    explicit owned_ptr(T &&object) : _block{Ptr{new Block{0, T{std::move(object)}}, &owned_ptr<T>::deleter}} {
+struct owned_ptr_error_handler {
+    static void check_condition(bool condition, const char *reason) {
+        (void) reason;
+        assert(condition);
     }
 
-    operator T*() { // NOLINT
+    static constexpr bool reset_moved_from_dep_ptr{false};
+};
+
+template<typename T, class ErrorHandler>
+class dep_ptr;
+
+template<typename T, class ErrorHandler = owned_ptr_error_handler>
+class owned_ptr {
+public:
+    explicit owned_ptr(T &&object) : _block{
+            Ptr{new Block{0, T{std::move(object)}}, &owned_ptr<T, ErrorHandler>::deleter}} {
+    }
+
+    operator T *() { // NOLINT
         return &_block->object;
     }
 
     template<class... Args>
-    static inline auto make(Args&&... args)
-    {
+    static inline auto make(Args &&... args) {
         auto p = new Block{0, T(std::forward<Args>(args)...)};
-        return owned_ptr(Ptr{p, &owned_ptr<T>::deleter});
+        return owned_ptr(Ptr{p, &owned_ptr<T, ErrorHandler>::deleter});
+    }
+
+    auto make_dep() {
+        return dep_ptr<T, ErrorHandler>{*this};
     }
 
 private:
@@ -36,70 +47,83 @@ private:
         T object;
 
         ~Block() {
+            ErrorHandler::check_condition(ref_count == 0, "One or more remaining dep_ptr when deleting owned_ptr");
             assert(ref_count == 0);
         }
     };
 
-    using Deleter = void(*)(Block*);
+    using Deleter = void (*)(Block *);
     using Ptr = std::unique_ptr<Block, Deleter>;
 
     Ptr _block;
 
-    explicit owned_ptr(Ptr&& block) : _block{std::move(block)} {}
+    explicit owned_ptr(Ptr &&block) : _block{std::move(block)} {}
 
-    static void deleter(Block* b) {
+    static void deleter(Block *b) {
         b->~Block();
     }
 
-    friend class dep_ptr<T>;
+    friend class dep_ptr<T, ErrorHandler>;
 };
 
 template<class T, class... Args>
-inline auto make_owned(Args&&... args)
-{
-    return owned_ptr<T>::make(std::forward<Args>(args)...);
+inline auto make_owned(Args &&... args) {
+    return owned_ptr<T, owned_ptr_error_handler>::make(std::forward<Args>(args)...);
 }
 
-template<typename T>
-class dep_ptr
-{
+template<typename T, class ErrorHandler>
+class dep_ptr {
 public:
-    explicit dep_ptr(owned_ptr<T>& owned) : _block{*owned._block.get()} {
-        _block.ref_count++;
+    explicit dep_ptr(owned_ptr<T, ErrorHandler> &owned) : _block{owned._block.get()} {
+        _block->ref_count++;
     }
 
-    dep_ptr(const dep_ptr& other) : _block{other._block} {
-        _block.ref_count++;
+    dep_ptr(const dep_ptr &other) : _block{other._block} {
+        _block->ref_count++;
     }
 
-    // ToDo: Consider setting _block to nullptr on moves
-
-    dep_ptr(dep_ptr&& other) noexcept : _block{other._block} {
-        _block.ref_count++;
+    dep_ptr &operator=(const dep_ptr &other) {
+        dep_ptr tmp(other);
+        swap(*this, tmp);
+        return *this;
     }
 
-    dep_ptr& operator=(const dep_ptr& other) {
-        _block.ref_count--;
-        _block = other;
-        _block.ref_count++;
+    dep_ptr(dep_ptr &&other) noexcept: _block{other._block} {
+        if constexpr (ErrorHandler::reset_moved_from_dep_ptr) {
+            other._block = nullptr;
+        } else {
+            _block->ref_count++;
+        }
     }
 
-    dep_ptr& operator=(dep_ptr&& other) noexcept {
-        _block.ref_count--;
-        _block = other;
-        _block.ref_count++;
+    dep_ptr &operator=(dep_ptr &&other) noexcept {
+        dep_ptr tmp(std::move(other));
+        swap(*this, tmp);
+        return *this;
     }
 
     ~dep_ptr() {
-        _block.ref_count--;
+        if constexpr (ErrorHandler::reset_moved_from_dep_ptr) {
+            if (!_block) {
+                return;
+            }
+        }
+        _block->ref_count--;
     }
 
-    operator T*() { // NOLINT
-        return &_block.object;
+    operator T *() { // NOLINT
+        if constexpr (ErrorHandler::reset_moved_from_dep_ptr) {
+            ErrorHandler::check_condition(_block, "dep_ptr has been moved from");
+        }
+        return &_block->object;
     }
 
 private:
-    typename owned_ptr<T>::Block& _block;
+    typename owned_ptr<T, ErrorHandler>::Block *_block;
+
+    static void swap(dep_ptr& a, dep_ptr& b) {
+        std::swap(a._block, b._block);
+    }
 };
 
 #endif //OWNED_PTR_OWNED_PTR_H
--- test/owned_ptr_test.cpp
+++ test/owned_ptr_test.cpp
@@ -13,13 +13,30 @@
 
 using namespace std;
 
+struct custom_error_handler {
+    static void check_condition(bool condition, const char* reason) {
+        (void)reason;
+        assert(condition);
+    }
+
+    static constexpr bool reset_moved_from_dep_ptr{true};
+};
+
 TEST(Basics, create)
 {
     owned_ptr<string> foo{string{"Foo"}};
     const string* s1 = foo;
-    const string* s2 = dep_ptr<string>{foo};
+    const string* s2 = foo.make_dep();
     ASSERT_EQ(*s1, "Foo");
     ASSERT_EQ(*s2, "Foo");
+}
+
+TEST(Basics, custom_error_handler)
+{
+    owned_ptr<string, custom_error_handler> foo{string{"Foo"}};
+    auto dep = dep_ptr<string, custom_error_handler>{foo};
+    ASSERT_EQ(*foo, "Foo");
+    ASSERT_EQ(*dep, "Foo");
 }
 
 TEST(Basics, rule_of_zero) {
